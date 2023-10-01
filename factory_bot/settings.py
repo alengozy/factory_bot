40b015a80e8ebba22c83707ca77331c06fed523a
AlenGozy <=>
2023-10-01 20:07:40 +0600

    Proper method name for urlparse.

--- factory_bot/settings.py
+++ factory_bot/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from urllib import parse as urlparse
+from urllib.parse import urlparse
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
